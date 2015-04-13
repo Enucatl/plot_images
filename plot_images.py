bbe0dbbfe972442ee29b29f5835cc2f35ee1f885
Matteo Abis <gmatteo..abis@gmail.com>
2015-04-13 11:18:38 +0200

    argparse -> click

--- plot_images.py
+++ plot_images.py
@@ -9,6 +9,7 @@ import numpy as np
 from scipy import stats
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import click
 
 
 pgf_with_rc_fonts = {
@@ -30,11 +31,36 @@ pgf_with_rc_fonts = {
 mpl.rcParams.update(pgf_with_rc_fonts)
 
 
+@click.command()
+@click.argument("filename", type=click.Path(exists=True))
+@click.option("--height", type=float, default=6)
+@click.option("--language", type=click.Choice(["it", "en"]), default="it")
+@click.option("--batch", is_flag=True)
+@click.option("--big_crop", nargs=4, type=int, default=[0, -1, 0, -1])
+@click.option("--small_crop", nargs=4, type=int, default=[300, 550, 60, 130])
+@click.option("--format", default="eps")
+def main(filename, height, language, batch, big_crop, small_crop, format):
+    input_file = h5py.File(filename, "r")
+    dataset_name = "postprocessing/dpc_reconstruction"
+    min_x, max_x, min_y, max_y = big_crop
+    dataset = input_file[dataset_name]
+    print("original shape", dataset.shape)
+    dataset = dataset[0, min_y:max_y, min_x:max_x, ...]
+    print("cropped", dataset.shape)
+    absorption_image = dataset[..., 0]
+    differential_phase_image = dataset[..., 1]
+    visibility_reduction_image = dataset[..., 2]
+
+    draw(filename, height, absorption_image,
+         differential_phase_image, visibility_reduction_image,
+         language, batch, small_crop, format)
+
+
 def draw(input_file_name, height,
          absorption_image,
          differential_phase_image,
          dark_field_image,
-         language="it", batch=True, magnified_square=[300, 550, 60, 130]):
+         language, batch, magnified_square, format):
     """Display the calculated images with matplotlib."""
     if language == "it":
         absorption_image_title = "assorbimento"
@@ -97,7 +123,6 @@ def draw(input_file_name, height,
         differential_phase_image[min_y:max_y, min_x:max_x], aspect='auto')
     limits = stats.mstats.mquantiles(differential_phase_image,
                                      prob=[0.02, 0.98])
-    #limits = (-3, 3)
     phase1_plot.set_ylabel(differential_phase_image_title,
                            size="large")
     phase1_plot.set_frame_on(False)
@@ -141,8 +166,9 @@ def draw(input_file_name, height,
                                      prob=[0.02, 0.98])
     df1.set_clim(*limits)
     df2.set_clim(*limits)
-    plt.savefig('images_{0}.eps'.format(
-        os.path.splitext(os.path.basename(input_file_name))[0]),
+    plt.savefig('images_{0}.{1}'.format(
+        os.path.splitext(os.path.basename(input_file_name))[0],
+        format),
         bbox_inches="tight", dpi=300)
     if not batch:
         plt.ion()
@@ -151,56 +177,4 @@ def draw(input_file_name, height,
 
 
 if __name__ == '__main__':
-    import argparse
-    commandline_parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    commandline_parser.add_argument(
-        "--language",
-        default="it",
-        choices=["it", "en"],
-        help="language for the text")
-    commandline_parser.add_argument(
-        "file",
-        nargs=1,
-        help="input file name")
-    commandline_parser.add_argument(
-        "height",
-        nargs="?",
-        default=6,
-        type=float,
-        help="height of the plot")
-    commandline_parser.add_argument(
-        "--small_crop",
-        nargs="*",
-        default=[300, 550, 60, 130],
-        type=int,
-        help="area inside the red rectangle (min_x, max_x, min_y, max_y)")
-    commandline_parser.add_argument(
-        "--big_crop",
-        nargs="*",
-        default=[0, -1, 0, -1],
-        type=int,
-        help="area inside the plot (min_x, max_x, min_y, max_y)")
-    commandline_parser.add_argument(
-        "--batch",
-        action="store_true",
-        help="run in batch mode, no picture is shown")
-    args = commandline_parser.parse_args()
-    input_file_name = args.file[0]
-    height = args.height
-
-    if not os.path.exists(input_file_name):
-        raise(OSError("{0} not found".format(input_file_name)))
-
-    input_file = h5py.File(input_file_name, "r")
-    dataset_name = "postprocessing/dpc_reconstruction"
-    min_x, max_x, min_y, max_y = args.big_crop
-    dataset = input_file[dataset_name][0, min_y:max_y, min_x:max_x, ...]
-    print(dataset.shape)
-    absorption_image = dataset[..., 0]
-    differential_phase_image = dataset[..., 1]
-    visibility_reduction_image = dataset[..., 2]
-
-    draw(input_file_name, height, absorption_image,
-         differential_phase_image, visibility_reduction_image,
-         args.language, args.batch, args.small_crop)
+    main()
