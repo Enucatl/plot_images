96852345fc2a18132791364db058ddbcdfd6500c
Matteo Abis <gmatteo..abis@gmail.com>
2016-04-21 09:13:42 +0200

    add plot big and plot ratio

--- plot_big.py
+++ plot_big.py
@@ -36,9 +36,8 @@ mpl.rcParams.update(pgf_with_rc_fonts)
 @click.option("--language", type=click.Choice(["it", "en"]), default="it")
 @click.option("--batch", is_flag=True)
 @click.option("--big_crop", nargs=4, type=int, default=[0, -1, 0, -1])
-@click.option("--small_crop", nargs=4, type=int, default=[300, 550, 60, 130])
 @click.option("--format", default="eps")
-def main(filename, height, language, batch, big_crop, small_crop, format):
+def main(filename, height, language, batch, big_crop, format):
     input_file = h5py.File(filename, "r")
     dataset_name = "postprocessing/dpc_reconstruction"
     min_x, max_x, min_y, max_y = big_crop
@@ -52,14 +51,14 @@ def main(filename, height, language, batch, big_crop, small_crop, format):
 
     draw(filename, height, absorption_image,
          differential_phase_image, visibility_reduction_image,
-         language, batch, small_crop, format)
+         language, batch, format)
 
 
 def draw(input_file_name, height,
          absorption_image,
          differential_phase_image,
          dark_field_image,
-         language, batch, magnified_square, format):
+         language, batch, format):
     """Display the calculated images with matplotlib."""
     if language == "it":
         absorption_image_title = "assorbimento"
@@ -69,57 +68,33 @@ def draw(input_file_name, height,
         absorption_image_title = "absorption"
         differential_phase_image_title = "differential phase"
         dark_field_image_title = "dark field"
-    _, ((abs1_plot, abs2_plot),
-        (phase1_plot, phase2_plot),
-        (df1_plot, df2_plot)) = plt.subplots(
-            3, 2, figsize=(6, height), dpi=300)
+    _, (abs1_plot,
+        phase1_plot,
+        df1_plot) = plt.subplots(
+            3, 1, figsize=(6, height), dpi=300)
     plt.subplots_adjust(
         wspace=0.02,
         hspace=0.02)
-    min_x, max_x, min_y, max_y = magnified_square
     abs1 = abs1_plot.imshow(absorption_image,
                             cmap=plt.cm.Greys,
                             aspect='auto')
-    abs1_plot.add_patch(mpl.patches.Rectangle(
-        (min_x, min_y),
-        max_x - min_x,
-        max_y - min_y,
-        fill=False,
-        edgecolor="r"))
-    abs2 = abs2_plot.imshow(
-        absorption_image[min_y:max_y, min_x:max_x],
-        cmap=plt.cm.Greys, aspect='auto')
     abs1_plot.set_ylabel(absorption_image_title,
                          size="large")
     abs1_plot.set_frame_on(False)
     abs1_plot.axes.yaxis.set_ticks([])
     abs1_plot.axes.xaxis.set_ticks([])
-    abs2_plot.axes.yaxis.set_ticks([])
-    abs2_plot.axes.xaxis.set_ticks([])
-    for pos in ["top", "bottom", "left", "right"]:
-        abs2_plot.spines[pos].set_color("r")
-    abs2_plot.axes.xaxis.set_ticks([])
     limits = stats.mstats.mquantiles(
         absorption_image,
         prob=[0.02, 0.98])
     limits = stats.mstats.mquantiles(absorption_image,
                                      prob=[0.02, 0.98])
     abs1.set_clim(*limits)
-    abs2.set_clim(*limits)
     print(limits)
-    plt.colorbar(abs2,
-                 ax=abs2_plot,
+    plt.colorbar(abs1,
+                 ax=abs1_plot,
                  format="% .2f",
                  ticks=np.arange(0, 1, 0.1).tolist())
     phase1 = phase1_plot.imshow(differential_phase_image, aspect='auto')
-    phase1_plot.add_patch(mpl.patches.Rectangle(
-        (min_x, min_y),
-        max_x - min_x,
-        max_y - min_y,
-        fill=False,
-        edgecolor="r"))
-    phase2 = phase2_plot.imshow(
-        differential_phase_image[min_y:max_y, min_x:max_x], aspect='auto')
     limits = stats.mstats.mquantiles(differential_phase_image,
                                      prob=[0.02, 0.98])
     phase1_plot.set_ylabel(differential_phase_image_title,
@@ -127,44 +102,24 @@ def draw(input_file_name, height,
     phase1_plot.set_frame_on(False)
     phase1_plot.axes.yaxis.set_ticks([])
     phase1_plot.axes.xaxis.set_ticks([])
-    phase2_plot.axes.yaxis.set_ticks([])
-    phase2_plot.axes.xaxis.set_ticks([])
-    for pos in ["top", "bottom", "left", "right"]:
-        phase2_plot.spines[pos].set_color("r")
-    phase2_plot.axes.xaxis.set_ticks([])
-    plt.colorbar(phase2,
-                 ax=phase2_plot,
+    plt.colorbar(phase1,
+                 ax=phase1_plot,
                  format="% .1f",
                  ticks=np.arange(-0.4, 0.4, 0.1).tolist())
     phase1.set_clim(*limits)
-    phase2.set_clim(*limits)
     df1 = df1_plot.imshow(dark_field_image, aspect='auto')
-    df1_plot.add_patch(mpl.patches.Rectangle(
-        (min_x, min_y),
-        max_x - min_x,
-        max_y - min_y,
-        fill=False,
-        edgecolor="r"))
-    df2 = df2_plot.imshow(
-        dark_field_image[min_y:max_y, min_x:max_x], aspect='auto')
     df1_plot.set_ylabel(dark_field_image_title,
                         size="large")
     df1_plot.set_frame_on(False)
     df1_plot.axes.yaxis.set_ticks([])
     df1_plot.axes.xaxis.set_ticks([])
-    df2_plot.axes.yaxis.set_ticks([])
-    df2_plot.axes.xaxis.set_ticks([])
-    for pos in ["top", "bottom", "left", "right"]:
-        df2_plot.spines[pos].set_color("r")
-    df2_plot.axes.xaxis.set_ticks([])
-    plt.colorbar(df2,
-                 ax=df2_plot,
+    plt.colorbar(df1,
+                 ax=df1_plot,
                  format="% .1f",
                  ticks=np.arange(0, 1, 0.1).tolist())
     limits = stats.mstats.mquantiles(dark_field_image,
                                      prob=[0.02, 0.98])
     df1.set_clim(*limits)
-    df2.set_clim(*limits)
     plt.savefig('images_{0}.{1}'.format(
         os.path.splitext(os.path.basename(input_file_name))[0],
         format),
--- plot_ratio.py
+++ plot_ratio.py
@@ -33,7 +33,7 @@ mpl.rcParams.update(pgf_with_rc_fonts)
 @click.command()
 @click.argument("filename", type=click.Path(exists=True))
 @click.option("--height", type=float, default=6)
-@click.option("--language", type=click.Choice(["it", "en"]), default="it")
+@click.option("--language", type=click.Choice(["it", "en"]), default="en")
 @click.option("--batch", is_flag=True)
 @click.option("--big_crop", nargs=4, type=int, default=[0, -1, 0, -1])
 @click.option("--small_crop", nargs=4, type=int, default=[300, 550, 60, 130])
@@ -47,97 +47,97 @@ def main(filename, height, language, batch, big_crop, small_crop, format):
     dataset = dataset[0, min_y:max_y, min_x:max_x, ...]
     print("cropped", dataset.shape)
     absorption_image = dataset[..., 0]
-    differential_phase_image = dataset[..., 1]
     visibility_reduction_image = dataset[..., 2]
+    ratio_image = (
+        np.log(visibility_reduction_image) /
+        np.log(absorption_image)
+    )
 
     draw(filename, height, absorption_image,
-         differential_phase_image, visibility_reduction_image,
+         visibility_reduction_image, ratio_image,
          language, batch, small_crop, format)
 
 
 def draw(input_file_name, height,
          absorption_image,
-         differential_phase_image,
          dark_field_image,
-         language, batch, magnified_square, format):
+         ratio_image,
+         language, batch,
+         magnified_square, format):
     """Display the calculated images with matplotlib."""
     if language == "it":
         absorption_image_title = "assorbimento"
-        differential_phase_image_title = "fase differenziale"
         dark_field_image_title = "riduzione di visibilit\\`a"
+        ratio_image_title = "rapporto dei logaritmi"
     else:
         absorption_image_title = "absorption"
-        differential_phase_image_title = "differential phase"
-        dark_field_image_title = "dark field"
+        ratio_image_title = "ratio of the logs"
+        dark_field_image_title = "visibility reduction"
     _, ((abs1_plot, abs2_plot),
-        (phase1_plot, phase2_plot),
-        (df1_plot, df2_plot)) = plt.subplots(
+        (df1_plot, df2_plot),
+        (ratio1_plot, ratio2_plot)) = plt.subplots(
             3, 2, figsize=(6, height), dpi=300)
     plt.subplots_adjust(
-        wspace=0.02,
-        hspace=0.02)
+        wspace=0.1,
+        hspace=0.2)
     min_x, max_x, min_y, max_y = magnified_square
-    abs1 = abs1_plot.imshow(absorption_image,
-                            cmap=plt.cm.Greys,
-                            aspect='auto')
+    abs1 = abs1_plot.imshow(
+        absorption_image,
+        cmap=plt.cm.Greys,
+        aspect='auto')
     abs1_plot.add_patch(mpl.patches.Rectangle(
         (min_x, min_y),
         max_x - min_x,
         max_y - min_y,
         fill=False,
         edgecolor="r"))
-    abs2 = abs2_plot.imshow(
-        absorption_image[min_y:max_y, min_x:max_x],
-        cmap=plt.cm.Greys, aspect='auto')
+    absorption_image_cropped = absorption_image[min_y:max_y, min_x:max_x]
+    abs2 = abs2_plot.hist(
+        absorption_image_cropped.flatten(),
+        50, facecolor='blue',
+        )
     abs1_plot.set_ylabel(absorption_image_title,
                          size="large")
     abs1_plot.set_frame_on(False)
     abs1_plot.axes.yaxis.set_ticks([])
     abs1_plot.axes.xaxis.set_ticks([])
-    abs2_plot.axes.yaxis.set_ticks([])
-    abs2_plot.axes.xaxis.set_ticks([])
-    for pos in ["top", "bottom", "left", "right"]:
-        abs2_plot.spines[pos].set_color("r")
-    abs2_plot.axes.xaxis.set_ticks([])
     limits = stats.mstats.mquantiles(
         absorption_image,
         prob=[0.02, 0.98])
     limits = stats.mstats.mquantiles(absorption_image,
                                      prob=[0.02, 0.98])
     abs1.set_clim(*limits)
-    abs2.set_clim(*limits)
     print(limits)
-    plt.colorbar(abs2,
-                 ax=abs2_plot,
+    plt.colorbar(abs1,
+                 ax=abs1_plot,
                  format="% .2f",
                  ticks=np.arange(0, 1, 0.1).tolist())
-    phase1 = phase1_plot.imshow(differential_phase_image, aspect='auto')
-    phase1_plot.add_patch(mpl.patches.Rectangle(
+    ratio1 = ratio1_plot.imshow(ratio_image, aspect='auto')
+    ratio1_plot.add_patch(mpl.patches.Rectangle(
         (min_x, min_y),
         max_x - min_x,
         max_y - min_y,
         fill=False,
         edgecolor="r"))
-    phase2 = phase2_plot.imshow(
-        differential_phase_image[min_y:max_y, min_x:max_x], aspect='auto')
-    limits = stats.mstats.mquantiles(differential_phase_image,
-                                     prob=[0.02, 0.98])
-    phase1_plot.set_ylabel(differential_phase_image_title,
+    ratio_image_cropped = ratio_image[min_y:max_y, min_x:max_x]
+    ratio2 = ratio2_plot.hist(
+        ratio_image_cropped.flatten(),
+        50, facecolor='blue',
+        )
+    limits = stats.mstats.mquantiles(
+        ratio_image_cropped,
+        prob=[0.02, 0.98])
+    print("ratio of logs, limits:", limits)
+    ratio1_plot.set_ylabel(ratio_image_title,
                            size="large")
-    phase1_plot.set_frame_on(False)
-    phase1_plot.axes.yaxis.set_ticks([])
-    phase1_plot.axes.xaxis.set_ticks([])
-    phase2_plot.axes.yaxis.set_ticks([])
-    phase2_plot.axes.xaxis.set_ticks([])
-    for pos in ["top", "bottom", "left", "right"]:
-        phase2_plot.spines[pos].set_color("r")
-    phase2_plot.axes.xaxis.set_ticks([])
-    plt.colorbar(phase2,
-                 ax=phase2_plot,
+    ratio1_plot.set_frame_on(False)
+    ratio1_plot.axes.yaxis.set_ticks([])
+    ratio1_plot.axes.xaxis.set_ticks([])
+    plt.colorbar(ratio1,
+                 ax=ratio1_plot,
                  format="% .1f",
-                 ticks=np.arange(-0.4, 0.4, 0.1).tolist())
-    phase1.set_clim(*limits)
-    phase2.set_clim(*limits)
+                 ticks=np.arange(*limits, 0.5).tolist())
+    ratio1.set_clim(*limits)
     df1 = df1_plot.imshow(dark_field_image, aspect='auto')
     df1_plot.add_patch(mpl.patches.Rectangle(
         (min_x, min_y),
@@ -145,27 +145,24 @@ def draw(input_file_name, height,
         max_y - min_y,
         fill=False,
         edgecolor="r"))
-    df2 = df2_plot.imshow(
-        dark_field_image[min_y:max_y, min_x:max_x], aspect='auto')
+    dark_field_image_cropped = dark_field_image[min_y:max_y, min_x:max_x]
+    df2 = df2_plot.hist(
+        dark_field_image_cropped.flatten(),
+        50, facecolor='blue',
+        )
     df1_plot.set_ylabel(dark_field_image_title,
                         size="large")
     df1_plot.set_frame_on(False)
     df1_plot.axes.yaxis.set_ticks([])
     df1_plot.axes.xaxis.set_ticks([])
-    df2_plot.axes.yaxis.set_ticks([])
-    df2_plot.axes.xaxis.set_ticks([])
-    for pos in ["top", "bottom", "left", "right"]:
-        df2_plot.spines[pos].set_color("r")
-    df2_plot.axes.xaxis.set_ticks([])
-    plt.colorbar(df2,
-                 ax=df2_plot,
+    plt.colorbar(df1,
+                 ax=df1_plot,
                  format="% .1f",
                  ticks=np.arange(0, 1, 0.1).tolist())
     limits = stats.mstats.mquantiles(dark_field_image,
                                      prob=[0.02, 0.98])
     df1.set_clim(*limits)
-    df2.set_clim(*limits)
-    plt.savefig('images_{0}.{1}'.format(
+    plt.savefig('ratio_{0}.{1}'.format(
         os.path.splitext(os.path.basename(input_file_name))[0],
         format),
         bbox_inches="tight", dpi=300)
